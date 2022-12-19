38ab4c08e99dc70d206ef24a707e34df18b7ebb1
Bruno <b.castro.doute@gmail.com>
2022-12-19 13:14:35 -0300

    Login

---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <img src="assets/imgs/TrackIt.png"></img>
+      <Input type="email">E-mail</Input>;<Input type="text">Senha</Input>
+      <Entrar>
+        {" "}
+        <Link to="/habitos"></Link>
+      </Entrar>{" "}
+      ;
+      <Cadastro>
+        {" "}
+        <Link to="/cadastro"> Não tem uma conta? Cadastre-se! </Link>
+      </Cadastro>
+    </>
+  );
+};
+
+export default Login;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  position: absolute;
+  width: 303px;
+  height: 45px;
+  background: #ffffff;
+  border: 1px solid #d5d5d5;
+  border-radius: 5px;
+`;
+
+const Entrar = styled.button`
+  width: 303px;
+  height: 45px;
+  background: #52b6ff;
+  border-radius: 4.63636px;
+`;
+
+const Cadastro = styled.p`
+  width: 232px;
+  height: 17px;
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13.976px;
+  line-height: 17px;
+  text-align: center;
+  text-decoration-line: underline;
+  color: #52b6ff;
+`;
